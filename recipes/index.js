a14667bc0c3d82f2fb1a2229e7c50640f323e41f
spencera321 <spencerabramson221@gmail.com>
2022-04-12 13:26:09 -0400

    Add the rest of files for recipes site
    This was done poorly as there should have been more commits in smaller batches

---
+++ recipes/index.js
@@ -0,0 +1,11 @@
+/*
+* Todo: Dynamically inject list items into the <ol id="my-list">
+* 1) how do I target the ol element by id?
+  2) how to I insert HTML into it?
+  3) how do I do this as a loop?
+  
+*   <li>Throw that shit in a food processor</li>
+    <li>???</li>
+    <li>???</li>
+    <li>Eat it up</li>
+*/
